0794361255d06b71946e53f46cdcbd1078d4888a
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2022-01-22 15:11:14 +0100

    finish base functionality

---
+++ datatable.py
@@ -0,0 +1,116 @@
+import sqlalchemy
+
+COLS_IGNORE_LIST = ["_sa_instance_state"]
+
+class DataTable():
+    
+    def __init__(self, data, db, dbClass, searchHelperClass):
+
+        self.db                = db
+        self.draw              = int(data["draw"])
+        self.start             = int(data["start"])
+        self.length            = int(data["length"])
+        self.trueLength        = -1
+        self.dbClass           = dbClass
+        self.searchHelperClass = searchHelperClass
+        self.orderByCol        = int(data["order[0][column]"])
+        self.searchValue       = data["search[value]"]
+        self.searchIsRegex     = data["search[regex]"]
+        self.orderDirection    = data["order[0][dir]"]
+
+        self.cols = DataTable.staticGetCols(dbClass)
+
+        # order variable for use with python sorted etc #
+        self.orderAsc = self.orderDirection == "asc"
+
+        # oder variable for use with sqlalchemy
+        if self.orderAsc:
+            self.orderAscDbClass = sqlalchemy.asc
+        else:
+            self.orderAscDbClass = sqlalchemy.desc
+
+    def staticGetCols(dbClass):
+        cols = list(dbClass.__table__.columns.keys())
+        return list(filter(lambda c: c not in COLS_IGNORE_LIST, cols))
+
+    def __build(self, results, total, filtered):
+
+        self.cacheResults = results
+        
+        count = 0
+        resultDicts = [ r.__dict__ for r in results ]
+
+        # data list must have the correct order (same as table scheme) #
+        rows = []
+        for r in resultDicts:
+            singleRow = []
+            for key in self.cols:
+                singleRow.append(r[key])
+            rows.append(singleRow)
+
+        d = dict()
+        d.update({ "draw" : self.draw })
+        d.update({ "recordsTotal" : total })
+        d.update({ "recordsFiltered" :  filtered })
+        d.update({ "data" : rows })
+
+        return d
+
+    def get(self):
+
+        filtered = 0
+        total    = 0
+
+        if self.searchValue:
+
+            # base query
+            query = self.db.session.query(self.searchHelperClass.uid)
+            total = query.count()
+
+            # search string (search for all substrings individually #
+            filterQuery = query
+            for substr in self.searchValue.split(" "):
+                searchSubstr = "%{}%".format(substr.strip())
+                filterQuery  = filterQuery.filter(
+                                    self.searchHelperClass.fullstring.like(searchSubstr))
+
+            # uidList  = filterQuery.all()
+            filtered = filterQuery.count()
+
+            # get relevant pIds from searchHelper #
+            uidList = filterQuery.offset(self.start).limit(self.length).all()
+
+            # use pIds to retrive full information #
+            results = []
+            for uidTup in uidList:
+                uid = uidTup[0]
+                singleResult = self.db.session.query(self.dbClass).filter(
+                                    self.dbClass.uid == uid).first()
+                if singleResult:
+                    results.append(singleResult)
+                
+                col = self.dbClass.__table__.c[self.orderByCol]
+                results = sorted(results, 
+                                    key=lambda row: self.getColByNumber(row, self.orderByCol), 
+                                    reverse=not self.orderAsc)
+        else:
+
+            query = self.db.session.query(self.dbClass)
+            if self.orderByCol:
+                query  = query.order_by(self.orderAscDbClass(
+                            list(self.dbClass.__table__.c)[self.orderByCol]))
+            else:
+                query  = query.order_by(self.orderAscDbClass(
+                            list(self.dbClass.__table__.c)[0]))
+
+            results  = query.offset(self.start).limit(self.length).all()
+            total    = query.count()
+            filtered = total
+
+        return self.__build(results, total, filtered)
+
+    def getColByNumber(self, dbClass, nr):
+        nr = int(nr)
+        value = getattr(dbClass, self.cols[nr])
+        return value
+
--- server.py
+++ server.py
@@ -8,25 +8,99 @@ import json
 import os
 import random
 import secrets
+import hashlib
+
+from sqlalchemy import Column, Integer, String, Boolean, or_, and_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import func
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+import datatable
 
 app = flask.Flask('Simple Log Server')
+db = SQLAlchemy(app)
 
-@app.route('/')
+class SearchHelper(db.Model):
+    __tablename__ = "search_helper"
+    uid = Column(String, primary_key=True)
+    fullstring = Column(String)
+
+class LogLine(db.Model):
+    __tablename__ = "log_line"
+    uid         = Column(String, primary_key=True)
+    timestamp   = Column(String)
+    service     = Column(String)
+    host        = Column(String)
+    contentType = Column(String)
+    content     = Column(String)
+    severity    = Column(Integer)
+
+@app.route('/', methods=["GET", "POST"])
 def main():
+    if flask.request.method == "POST":
+        dt = datatable.DataTable(flask.request.form.to_dict(), db, LogLine, SearchHelper)
+        return flask.Response(json.dumps(dt.get()), 200, mimetype='application/json')
+    else:
+        return flask.render_template("index.html", 
+                        headerRow=datatable.DataTable.staticGetCols(LogLine))
 
-@app.route('/submitt', methods=["PUT"])
+
+@app.route('/submit', methods=["PUT"])
 def submitt():
 
     d = flask.request.json
 
-    service     = d["service"]
-    host        = d["host"]
-    contentType = d["contentType"]
-    content     = d["content"]
+    service     = d.get("service")
+    host        = d.get("host")
+    contentType = d.get("contentType")
+    content     = d.get("content")
+    severity    = d.get("severity")
+
+    # service #
+    if not service:
+        return ("Missing Service filed", 405)
+    elif not content:
+        return ("Missing Content or Content Empty", 405)
+
+    # parse content type #
+    if not contentType:
+        contentType = "simple"
+
+    # parse severity #
+    if not severity:
+        severity = -1
+    try:
+        severity = int(severity)
+    except ValueError:
+        severity = severityStringToInt(severity)
+
+    # parse host #
+    if not host:
+        host = "undefined"
+
+    timestamp = datetime.datetime.now().timestamp()
+    uid = hashlib.sha512((str(timestamp) + str(service) + str(host)).encode("UTF-8")).hexdigest()
+    ll = LogLine(uid=uid, timestamp=timestamp, service=service, host=host,
+                    contentType=contentType, content=content, severity=severity)
+
+    fullstring = "{uid} {timestamp} {service} {host} {contentType} {content} {severity}".format(
+                        uid=uid, timestamp=timestamp, service=service, host=host, 
+                        contentType=contentType, content=content, severity=severity)
+
+    sh = SearchHelper(uid=uid, fullstring=fullstring)
+    
+    db.session.add(ll)
+    db.session.add(sh)
+
+    db.session.commit()
+
+    return ("", 204)
 
 
 @app.route('/static/<path:path>')
-def static(path):
+def staticDir(path):
     return send_from_directory('static', path)
 
 @app.before_first_request
@@ -36,6 +110,9 @@ def init():
         app.config.from_object("config")
     else:
         print("Config not found!", file=sys.stderr)
+
+    app.config["DB"] = db
+    db.create_all()
 
 
 if __name__ == "__main__":
